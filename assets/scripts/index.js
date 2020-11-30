850b90758a8300558da7be5de0d3223c881a09c8
EC2 Default User <ec2-user@ip-172-31-87-62.ec2.internal>
2020-11-30 20:04:11 +0000

    change index-html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -17,3 +17,65 @@ function openTab(evt, cityName) {
 function openRedirect(url) {
     window.location.href = url;
 }
+
+function calcNum() {
+
+    var operandOne;
+    var operandTwo;
+    var oneFp;
+    var twoFp;
+    var addResult;
+    var subtractResult;
+    var multiplyResult;
+    var divideResult;
+
+    operandOne = document.getElementById("numOne").value;
+    operandTwo = document.getElementById("numTwo").value;
+
+    oneFp = parseFloat(operandOne);
+    twoFp = parseFloat(operandTwo);
+
+    if ($("#calcForm").validate()) {
+        if (document.getElementById("rdAdd").checked) {
+            addResult = oneFp + twoFp;
+            document.getElementById("calcValue").innerHTML = addResult.toString();
+        }
+        else if (document.getElementById("rdSubtract").checked) {
+            subtractResult = oneFp - twoFp;
+            document.getElementById("calcValue").innerHTML = subtractResult.toString();
+        }
+        else if (document.getElementById("rdMultiply").checked) {
+            multiplyResult = oneFp * twoFp;
+            document.getElementById("calcValue").innerHTML = multiplyResult.toString();
+        }
+        else if (document.getElementById("rdDivide").checked) {
+            divideResult = oneFp / twoFp;
+            document.getElementById("calcValue").innerHTML = divideResult.toString();
+        }
+    }
+}
+
+function clearform() {
+
+    document.getElementById("numOne").value = '';
+    $(".radioSelect").prop("checked", false);
+    document.getElementById("numTwo").value = '';
+    document.getElementById("calcValue").innerHTML = "";
+
+}
+
+$(function() {
+    $("#calcForm").validate({
+        rules: {
+            numOne: {
+                required: true,
+            },
+            rdMath: {
+                required: true,
+            },
+            numTwo: {
+                required: true,
+            }
+        }
+    });
+});
